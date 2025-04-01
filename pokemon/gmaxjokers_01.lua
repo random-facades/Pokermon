7412e5c9d6c672dd679fdab83c5c6f4e15c65515
random-facades <random.facades@gmail.com>
2025-04-01 07:06:00 -0400

    Gmax functionalities

--- pokemon/gmaxjokers_01.lua
+++ pokemon/gmaxjokers_01.lua
@@ -40,6 +40,17 @@ local gmax_list = {
       remove_from_deck = function(self, card, from_debuff)
          G.hand:change_size(-card.ability.extra.h_size)
       end,
+      calculate = function(self, card, context)
+         if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+               return {
+                  message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult_no_energy } },
+                  colour = G.C.XMULT,
+                  Xmult_mod = card.ability.extra.Xmult_no_energy
+               }
+            end
+         end
+      end,
    },
    {
       name = "gmax_charizard",
@@ -59,7 +70,18 @@ local gmax_list = {
       remove_from_deck = function(self, card, from_debuff)
          G.GAME.round_resets.discards = G.GAME.round_resets.discards - card.ability.extra.d_size
          ease_discard(-card.ability.extra.d_size)
-      end
+      end,
+      calculate = function(self, card, context)
+         if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+               return {
+                  message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult_no_energy } },
+                  colour = G.C.XMULT,
+                  Xmult_mod = card.ability.extra.Xmult_no_energy
+               }
+            end
+         end
+      end,
    },
    {
       name = "gmax_blastoise",
@@ -83,6 +105,17 @@ local gmax_list = {
             ease_hands_played(-to_decrease)
          end
       end,
+      calculate = function(self, card, context)
+         if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+               return {
+                  message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult_no_energy } },
+                  colour = G.C.XMULT,
+                  Xmult_mod = card.ability.extra.Xmult_no_energy
+               }
+            end
+         end
+      end,
    },
    {
       name = "gmax_butterfree",
@@ -95,6 +128,7 @@ local gmax_list = {
       end,
       ptype = "Grass",
       blueprint_compat = true,
+      -- no calculate needed
    },
    {
       name = "gmax_pikachu",
@@ -107,30 +141,103 @@ local gmax_list = {
       end,
       ptype = "Lightning",
       blueprint_compat = true,
+      calculate = function(self, card, context)
+         if context.cardarea == G.jokers and context.full_hand and context.after then
+            local basic_cards = {}
+            local non_gold_enhanced = {}
+            for k, v in pairs(G.hand.cards) do
+               if v.ability.set ~= 'Enhanced' then
+                  table.insert(basic_cards, v)
+               elseif not SMODS.has_enhancement(v, 'm_gold') then
+                  table.insert(non_gold_enhanced, v)
+               end
+            end
+            local target = nil
+            if #basic_cards > 0 then
+               target = pseudorandom_element(basic_cards, pseudoseed('gmax_pikachu'))
+            elseif #non_gold_enhanced > 0 then
+               target = pseudorandom_element(non_gold_enhanced, pseudoseed('gmax_pikachu'))
+            end
+            if target then
+               target:set_ability(G.P_CENTERS.m_gold, nil, true)
+               G.E_MANAGER:add_event(Event({
+                  func = function()
+                     target:juice_up()
+                     return true
+                  end
+               }))
+               return { message = localize('k_gold') }
+            end
+         end
+      end,
    },
    {
       name = "gmax_meowth",
       pos = { x = 5, y = 0 },
       soul_pos = { x = 5, y = 3 },
-      config = { extra = {} },
+      config = { extra = { money = 10, triggers_left = 2, triggers_max = 2 } },
       loc_vars = function(self, info_queue, card)
          type_tooltip(self, info_queue, card)
-         return { vars = {} }
+         return { vars = { card.ability.extra.money, card.ability.extra.triggers_left, card.ability.extra.triggers_max } }
       end,
       ptype = "Colorless",
       blueprint_compat = true,
+      calculate = function(self, card, context)
+         if context.cardarea == G.jokers and context.scoring_hand and context.final_final_scoring_step_v2 and card.ability.extra.triggers_left > 0 then
+            card.ability.extra.triggers_left = card.ability.extra.triggers_left - 1
+            mult = 0
+            hand_chips = 0
+            G.E_MANAGER:add_event(Event({
+               func = function()
+                  update_hand_text({ immediate = true, nopulse = true, delay = 0 },
+                     { mult = 0, chips = 0, level = '', handname = '' })
+                  return true
+               end
+            }))
+            ease_poke_dollars(card, "gmax_meowth", card.ability.extra.money or 10)
+            return {
+               message = localize('k_nope_ex')
+            }
+         end
+         if context.end_of_round then
+            card.ability.extra.triggers_left = card.ability.extra.triggers_max
+         end
+      end,
    },
    {
       name = "gmax_machamp",
       pos = { x = 6, y = 0 },
       soul_pos = { x = 6, y = 3 },
-      config = { extra = {} },
+      config = { extra = { hands = 8, blind_mult = 10 } },
       loc_vars = function(self, info_queue, card)
          type_tooltip(self, info_queue, card)
-         return { vars = {} }
+         return { vars = { card.ability.extra.hands, card.ability.extra.blind_mult } }
       end,
       ptype = "Fighting",
       blueprint_compat = true,
+      add_to_deck = function(self, card, from_debuff)
+         G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.hands
+         ease_hands_played(card.ability.extra.hands)
+         self:calculate(card, {setting_blind = true})
+      end,
+      remove_from_deck = function(self, card, from_debuff)
+         G.GAME.round_resets.hands = G.GAME.round_resets.hands - card.ability.extra.hands
+         local to_decrease = math.min(G.GAME.current_round.hands_left - 1, card.ability.extra.hands)
+         if to_decrease > 0 then
+            ease_hands_played(-to_decrease)
+         end
+      end,
+      calculate = function(self, card, context)
+         if context.setting_blind and G.GAME.blind and G.GAME.blind.in_blind then
+            G.E_MANAGER:add_event(Event({
+               func = function()
+                  G.GAME.blind.chips = G.GAME.blind.chips * card.ability.extra.blind_mult
+                  G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+                  return true
+               end
+            }))
+         end
+      end,
    },
    {
       name = "gmax_gengar",
@@ -143,6 +250,17 @@ local gmax_list = {
       end,
       ptype = "Psychic",
       blueprint_compat = true,
+      calculate = function(self, card, context)
+         if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+               return {
+                  message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult_no_energy } },
+                  colour = G.C.XMULT,
+                  Xmult_mod = card.ability.extra.Xmult_no_energy
+               }
+            end
+         end
+      end,
    },
    {
       name = "gmax_kingler",
@@ -474,13 +592,18 @@ for _, v in pairs(gmax_list) do
 
    v.poke_calculation = v.calculate
    v.calculate = function(self, card, context)
-      if self.poke_calculation then
-         local ret = self:poke_calculation(card, context)
-         if ret then return ret end
-      end
+      local ret = nil
       if can_evolve(self, card, context, self.pre_evo_name) then
-         level_evo(self, card, context, self.pre_evo_name)
+         ret = level_evo(self, card, context, self.pre_evo_name)
       end
+      if self.poke_calculation then
+         local calc = self:poke_calculation(card, context)
+         if calc then
+            calc.extra = ret
+            ret = calc
+         end
+      end
+      return ret
    end
 end
 
